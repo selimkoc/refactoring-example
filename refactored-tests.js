b9d1e8e59f8ffe97ee64011d83792bf2d2cdf483
Selim Koç <selimkoc@gmail.com>
2023-04-30 20:26:46 +0300

    Tests for Refactored Code

---
+++ refactored-tests.js
@@ -0,0 +1,101 @@
+const { type, typeGroup, typeInfo, typeGroupInfo, reportTitles, ExpensesLibrary }  = require('./refactored')
+
+/** TEST DATA */
+
+const ExpensesTestData1 = () => [
+  { type: type.LUNCH, amount: 10.2 },
+  { type: type.DINNER, amount: 16.0 },
+];
+
+const ExpensesTestData1Total = 26.2;
+const ExpensesTestData1Meals = 26.2;
+const ExpensesTestData1Rentals = 0;
+
+
+const ExpensesTestData2 = () => [
+  { type: type.BREAKFAST, amount: 15.2 },
+  { type: type.BREAKFAST, amount: 28.1 },
+  { type: type.LUNCH, amount: 10.2 },
+  { type: type.LUNCH, amount: 25.2 },
+  { type: type.DINNER, amount: 15.0 },
+  { type: type.DINNER, amount: 100.5 },
+  { type: type.CAR_RENTAL, amount: 150.0 },
+  { type: type.CAR_RENTAL, amount: 175.5 },
+];
+
+const ExpensesTestData2Total = 519.7;
+const ExpensesTestData2Meals = 194.2;
+const ExpensesTestData2Rentals = 325.5;
+
+const ExpensesTestData1And2Total = 545.9;
+
+/** TESTS */
+
+
+describe("Test getTotalExpense Function Exists", () => {
+  const testExpensesLibrary = new ExpensesLibrary(ExpensesTestData1());
+
+  test("defines getTotalExpense()", () => {
+    expect(typeof testExpensesLibrary.getTotalExpense).toBe("function");
+  });
+});
+
+
+describe("Test Total Values", () => {
+  const testExpensesLibrary = new ExpensesLibrary(ExpensesTestData1());
+
+  test("Test Calculation of Total Values", () => {
+        expect(testExpensesLibrary.getTotalExpense()).toEqual(ExpensesTestData1Total);
+  });
+});
+
+describe("Test Total Values With Second Input", () => {
+  const testExpensesLibrary = new ExpensesLibrary(ExpensesTestData2());
+
+  test("Test Calculation of Total Values", () => {
+        expect(testExpensesLibrary.getTotalExpense()).toEqual(ExpensesTestData2Total);
+  });
+});
+
+
+
+describe("Test Total Meals", () => {
+  const testExpensesLibrary = new ExpensesLibrary(ExpensesTestData1());
+
+  test("Calculation of Total Values", () => {
+        expect(testExpensesLibrary.getExpenseGroupsTotals(1)).toEqual(ExpensesTestData1Meals);
+  });
+});
+
+describe("Test Total Meals With Second Input", () => {
+  const testExpensesLibrary = new ExpensesLibrary(ExpensesTestData2());
+
+  test("Calculation of Total Values", () => {
+        expect(testExpensesLibrary.getExpenseGroupsTotals(1)).toEqual(ExpensesTestData2Meals);
+  });
+});
+
+describe("Test Total Rentals", () => {
+  const testExpensesLibrary = new ExpensesLibrary(ExpensesTestData1());
+
+  test("Calculation of Total Values", () => {
+        expect(testExpensesLibrary.getExpenseGroupsTotals(2)).toEqual(ExpensesTestData1Rentals);
+  });
+});
+
+describe("Test Total Rentals With Second Input", () => {
+  const testExpensesLibrary = new ExpensesLibrary(ExpensesTestData2());
+
+  test("Calculation of Total Values", () => {
+        expect(testExpensesLibrary.getExpenseGroupsTotals(2)).toEqual(ExpensesTestData2Rentals);
+  });
+});
+
+
+describe("Test Total Value Add Additional Input", () => {
+  const testExpensesLibrary = new ExpensesLibrary(ExpensesTestData1());
+testExpensesLibrary.addMoreExpenses(ExpensesTestData2());
+  test("Test  addMoreExpenses", () => {
+        expect(testExpensesLibrary.getTotalExpense()).toEqual(ExpensesTestData1And2Total);
+  });
+});
